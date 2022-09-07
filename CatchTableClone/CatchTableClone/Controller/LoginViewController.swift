99964d26728c5a26e54577525cc11a7b92414034
Jung SeokYoung <thrdud0423@gmail.com>
2022-09-07 16:51:57 +0900

    [FEA]: Apple Login
    add Apple Login UI

--- CatchTableClone/CatchTableClone/Controller/LoginViewController.swift
+++ CatchTableClone/CatchTableClone/Controller/LoginViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import SnapKit
 import KakaoSDKCommon
 import KakaoSDKUser
+import AuthenticationServices
 
 
 class LoginViewController: UIViewController {
@@ -27,7 +28,7 @@ class LoginViewController: UIViewController {
         loginLogoView.snp.makeConstraints { make in
             make.leading.equalToSuperview().offset(15)
             make.top.equalToSuperview()
-            make.height.equalTo(200)
+            make.height.equalTo(view.height/4)
             make.trailing.equalToSuperview()
         }
         loginView.snp.makeConstraints { make in
@@ -61,9 +62,17 @@ extension LoginViewController : LoginVieWDelegate{
             }
     }
     func appleLoginButtonPressed() {
-        let vc = UIViewController()
-        vc.view.backgroundColor = .systemBackground
-        self.present(vc, animated: true)
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+            
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+//        let vc = UIViewController()
+//        vc.view.backgroundColor = .systemBackground
+//        self.present(vc, animated: true)
     }
     
     func naverLoginButtonPressed() {
@@ -94,4 +103,37 @@ extension LoginViewController : LoginVieWDelegate{
         navigationController?.pushViewController(vc, animated: false)
     }
 
+}
+
+
+//MARK: - apple login
+extension LoginViewController : ASAuthorizationControllerPresentationContextProviding, ASAuthorizationControllerDelegate {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+    // Apple ID 연동 성공 시
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        switch authorization.credential {
+        // Apple ID
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                
+            // 계정 정보 가져오기
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+                
+            print("User ID : \(userIdentifier)")
+            print("User Email : \(email ?? "")")
+            print("User Name : \((fullName?.givenName ?? "") + (fullName?.familyName ?? ""))")
+     
+        default:
+            break
+        }
+    }
+        
+    // Apple ID 연동 실패 시
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // Handle error.
+    }
+    
 }
