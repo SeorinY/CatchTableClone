0a36c42c1c8bea62a5dbf0ddd921e3793f836b5a
유서린 <81281190+SeorinY@users.noreply.github.com>
2022-09-07 11:10:01 +0900

    [CLN] : Refectoring LoginViewController

--- CatchTableClone/CatchTableClone/Controller/LoginViewController1.swift
+++ CatchTableClone/CatchTableClone/Controller/LoginViewController1.swift
@@ -11,155 +11,56 @@ import SnapKit
 class LoginViewController1: UIViewController {
     private let loginHeaderView = LoginHeaderView()
 
-    private let alreadySignUpLabel : UILabel = {
-        let label = UILabel()
-        label.text = "이미 가입하셨나요?"
-        label.font = UIFont.systemFont(ofSize:15)
-        label.textAlignment = .center
-        label.textColor = .black
-        return label
-    }()
-    private let phoneNumLoginButton : UIButton = {
-        let button = UIButton()
-        button.setTitle("전화번호로 로그인 하기", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize:15)
-        button.layer.cornerRadius = 25
-        button.layer.borderWidth = 1
-        button.layer.borderColor = UIColor.lightGray.cgColor
-        return button
-    }()
+    private let signUpView = SignUpView()
     
-    private let inquireButton : UIButton = {
-        let button = UIButton()
-        button.setTitle("회원 가입 문의", for: .normal)
-        button.titleLabel?.attributedText = NSAttributedString(string: ".", attributes: [.underlineStyle: NSUnderlineStyle.single.rawValue])
-        button.titleLabel?.font = UIFont.systemFont(ofSize:15)
-        button.setTitleColor(.gray, for: .normal)
-        return button
-    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         setSubview()
-        setLayout()
-        setButton()
     }
     private func setSubview(){
         view.addSubview(loginHeaderView)
-        view.addSubview(alreadySignUpLabel)
-        view.addSubview(phoneNumLoginButton)
-        view.addSubview(inquireButton)
+        view.addSubview(signUpView)
+        signUpView.delegate = self
     }
     
-    private func setLayout(){
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         loginHeaderView.snp.makeConstraints({make in
             make.leading.equalToSuperview()
             make.top.equalToSuperview().offset(200)
+            make.height.equalTo(100)
         })
-        
-        
-        firstLine.snp.makeConstraints({make in
-            make.centerY.equalToSuperview()
-            make.leading.equalToSuperview().offset(20)
-            make.trailing.equalToSuperview().offset(-20)
-            make.height.equalTo(1)
+        signUpView.snp.makeConstraints({make in
+            make.top.equalTo(loginHeaderView.snp.bottom).offset(30)
+            make.bottom.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
         })
-        orStackView.snp.makeConstraints({make in
-            make.centerY.equalToSuperview()
-            make.centerX.equalToSuperview()
-            make.width.equalTo(100)
-            make.height.equalTo(20)
-        })
-        appleSignUpButton.snp.makeConstraints({make in
-            make.leading.equalToSuperview().offset(20)
-            make.trailing.equalToSuperview().offset(-20)
-            make.bottom.equalTo(orStackView.snp.top).offset(-40)
-            make.height.equalTo(50)
-        })
-
-        kakaoSignUpBUtton.snp.makeConstraints({make in
-            make.leading.equalToSuperview().offset(20)
-            make.trailing.equalToSuperview().offset(-20)
-            make.bottom.equalTo(appleSignUpButton.snp.top).offset(-10)
-            make.height.equalTo(50)
-        })
-        
-        naverSignUpButton.snp.makeConstraints({make in
-            make.centerX.equalToSuperview().offset(-60)
-            make.top.equalTo(firstLine.snp.bottom).offset(50)
-            make.width.equalTo(50)
-            make.height.equalTo(50)
-        })
-        
-        phoneNumSignUpButton.snp.makeConstraints({make in
-            make.centerX.equalToSuperview().offset(60)
-            make.top.equalTo(firstLine.snp.bottom).offset(50)
-            make.width.equalTo(50)
-            make.height.equalTo(50)
-        })
-        
-        secondLine.snp.makeConstraints({make in
-            make.centerX.equalToSuperview()
-            make.width.equalTo(1)
-            make.top.equalTo(firstLine.snp.bottom).offset(70)
-            make.height.equalTo(20)
-        })
-        
-        alreadySignUpLabel.snp.makeConstraints({make in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(secondLine.snp.bottom).offset(70)
-            make.height.equalTo(20)
-        })
-        
-        phoneNumLoginButton.snp.makeConstraints({make in
-            make.leading.equalToSuperview().offset(70)
-            make.trailing.equalToSuperview().offset(-70)
-            make.top.equalTo(alreadySignUpLabel.snp.bottom).offset(10)
-            make.height.equalTo(50)
-        })
-        
-        inquireButton.snp.makeConstraints({make in
-            make.centerX.equalToSuperview()
-//            make.top.equalTo(phoneNumLoginButton.snp.bottom).offset(100)
-            make.bottom.equalTo(additionalSafeAreaInsets.bottom).offset(0)
-            make.height.equalTo(15)
-        })
-    }
-    private func setButton(){
-        kakaoSignUpBUtton.addTarget(self, action: #selector(didTapKakaoLoginButton), for: .touchUpInside)
-        appleSignUpButton.addTarget(self, action: #selector(didTapAppleSignUpButton), for: .touchUpInside)
-        orButton.addTarget(self, action: #selector(didTapOrButton), for: .touchUpInside)
-        naverSignUpButton.addTarget(self, action: #selector(didTapNaverSignUpButton), for: .touchUpInside)
-        phoneNumSignUpButton.addTarget(self, action: #selector(didTapPhoneNumSignUpButton), for: .touchUpInside)
-        phoneNumLoginButton.addTarget(self, action: #selector(didTapPhoneNumLoginButton), for: .touchUpInside)
-        inquireButton.addTarget(self, action: #selector(didTapInquireButton), for: .touchUpInside)
-    }
-    @objc private func didTapKakaoLoginButton(){
-        DEBUG_LOG(1)
+    }    
+}
+extension LoginViewController1 : SignUpViewDelegate{
+    func kakaoLoginButtonAction() {
         let alert = UIAlertController(title: "'캐치테이블'에서 '카카오톡'을(를) 열려고 합니다.", message: "", preferredStyle: .alert)
-        DEBUG_LOG(2)
         alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
-        DEBUG_LOG(3)
         alert.addAction(UIAlertAction(title: "열기", style: .default) {_ in
             self.navigationController?.pushViewController(UIViewController(), animated: true)
         })
-        DEBUG_LOG(4)
         self.present(alert, animated: true)
-        DEBUG_LOG(5)
     }
-    @objc private func didTapAppleSignUpButton(){
+    func appleSignUpButtonAction() {
         let vc = UIViewController()
         vc.view.backgroundColor = .systemBackground
 //        vc.view.frame = CGRect(x: 0, y: view.height/2, width: view.width, height: view.height/2)
         self.present(vc, animated: true)
     }
-    @objc private func didTapOrButton(){
+    func orButtonAction() {
         let vc = UIViewController()
 //        vc.view.frame = CGRect(x: view.width/12, y: view.height/8, width: view.width*5/6, height: view.height*3/4)
         vc.view.backgroundColor = .systemBackground
         self.present(vc, animated: false)
     }
-    @objc private func didTapNaverSignUpButton(){
+
+    func naverSignUpButtonAction() {
         let alert = UIAlertController(title: "'캐치테이블'에서 'NAVER'을(를) 열려고 합니다.", message: "", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
         alert.addAction(UIAlertAction(title: "열기", style: .default) {_ in
@@ -168,17 +69,17 @@ class LoginViewController1: UIViewController {
         
         self.present(alert, animated: true)
     }
-    @objc private func didTapPhoneNumSignUpButton(){
+    func phoneNumSignUpButtonAction() {
         let vc = UIViewController()
         vc.view.frame = view.bounds
         navigationController?.pushViewController(vc, animated: false)
     }
-    @objc private func didTapPhoneNumLoginButton(){
+    func phoneNumLoginButtonAction() {
         let vc = UIViewController()
         vc.view.frame = view.bounds
         navigationController?.pushViewController(vc, animated: false)
     }
-    @objc private func didTapInquireButton(){
+    func inquireButtonAction() {
         let vc = UIViewController()
         vc.view.frame = view.bounds
         navigationController?.pushViewController(vc, animated: false)
--- CatchTableClone/CatchTableClone/View/SignUpView.swift
+++ CatchTableClone/CatchTableClone/View/SignUpView.swift
@@ -7,7 +7,17 @@
 
 import UIKit
 
+protocol SignUpViewDelegate : AnyObject{
+    func kakaoLoginButtonAction()
+    func appleSignUpButtonAction()
+    func orButtonAction()
+    func naverSignUpButtonAction()
+    func phoneNumSignUpButtonAction()
+    func phoneNumLoginButtonAction()
+    func inquireButtonAction()
+}
 class SignUpView: UIView {
+    public weak var delegate : SignUpViewDelegate?
     
     private let kakaoSignUpBUtton : UIButton = {
         let button = UIButton()
@@ -24,7 +34,6 @@ class SignUpView: UIView {
         button.layer.cornerRadius = 5
         button.layer.borderWidth = 1
         button.layer.borderColor = UIColor.lightGray.cgColor
-        
         
         let config = UIImage.SymbolConfiguration(pointSize: 25)
         let image = UIImage(systemName: "applelogo", withConfiguration: config)
@@ -88,8 +97,43 @@ class SignUpView: UIView {
         line.backgroundColor = .gray
         return line
     }()
+    
+    private let alreadySignUpLabel : UILabel = {
+        let label = UILabel()
+        label.text = "이미 가입하셨나요?"
+        label.font = UIFont.systemFont(ofSize:15)
+        label.textAlignment = .center
+        label.textColor = .black
+        return label
+    }()
+    private let phoneNumLoginButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("전화번호로 로그인 하기", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize:15)
+        button.layer.cornerRadius = 25
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        return button
+    }()
+    
+    private let inquireButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("회원 가입 문의", for: .normal)
+        button.titleLabel?.attributedText = NSAttributedString(string: ".", attributes: [.underlineStyle: NSUnderlineStyle.single.rawValue])
+        button.titleLabel?.font = UIFont.systemFont(ofSize:15)
+        button.setTitleColor(.gray, for: .normal)
+        return button
+    }()
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configureSubView()
+        configureButton()
+    }
+    
+    private func configureSubView(){
         addSubview(kakaoSignUpBUtton)
         addSubview(appleSignUpButton)
         
@@ -101,39 +145,70 @@ class SignUpView: UIView {
         addSubview(naverSignUpButton)
         addSubview(phoneNumSignUpButton)
         addSubview(secondLine)
+        addSubview(alreadySignUpLabel)
+        addSubview(phoneNumLoginButton)
+        addSubview(inquireButton)
     }
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+    private func configureButton(){
+        kakaoSignUpBUtton.addTarget(self, action: #selector(didTapKakaoLoginButton), for: .touchUpInside)
+        appleSignUpButton.addTarget(self, action: #selector(didTapAppleSignUpButton), for: .touchUpInside)
+        orButton.addTarget(self, action: #selector(didTapOrButton), for: .touchUpInside)
+        naverSignUpButton.addTarget(self, action: #selector(didTapNaverSignUpButton), for: .touchUpInside)
+        phoneNumSignUpButton.addTarget(self, action: #selector(didTapPhoneNumSignUpButton), for: .touchUpInside)
+        phoneNumLoginButton.addTarget(self, action: #selector(didTapPhoneNumLoginButton), for: .touchUpInside)
+        inquireButton.addTarget(self, action: #selector(didTapInquireButton), for: .touchUpInside)
+    }
+    @objc private func didTapKakaoLoginButton(){
+        delegate?.kakaoLoginButtonAction()
+    }
+    @objc private func didTapAppleSignUpButton(){
+        delegate?.appleSignUpButtonAction()
+    }
+    @objc private func didTapOrButton(){
+        delegate?.orButtonAction()
+    }
+    @objc private func didTapNaverSignUpButton(){
+        delegate?.naverSignUpButtonAction()
+    }
+    @objc private func didTapPhoneNumSignUpButton(){
+        delegate?.phoneNumSignUpButtonAction()
+    }
+    @objc private func didTapPhoneNumLoginButton(){
+        delegate?.phoneNumLoginButtonAction()
+    }
+    @objc private func didTapInquireButton(){
+        delegate?.inquireButtonAction()
+    }
+
     override func layoutSubviews() {
+        kakaoSignUpBUtton.snp.makeConstraints({make in
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.top.equalToSuperview()
+            make.height.equalTo(50)
+        })
+        appleSignUpButton.snp.makeConstraints({make in
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.top.equalTo(kakaoSignUpBUtton.snp.bottom).offset(10)
+            make.height.equalTo(50)
+        })
+        
         firstLine.snp.makeConstraints({make in
-            make.centerY.equalToSuperview()
+            make.centerY.equalTo(appleSignUpButton.snp.bottom).offset(40)
             make.leading.equalToSuperview().offset(20)
             make.trailing.equalToSuperview().offset(-20)
             make.height.equalTo(1)
         })
         orStackView.snp.makeConstraints({make in
-            make.centerY.equalToSuperview()
+            make.centerY.equalTo(appleSignUpButton.snp.bottom).offset(40)
             make.centerX.equalToSuperview()
             make.width.equalTo(100)
             make.height.equalTo(20)
         })
-        appleSignUpButton.snp.makeConstraints({make in
-            make.leading.equalToSuperview().offset(20)
-            make.trailing.equalToSuperview().offset(-20)
-            make.bottom.equalTo(orStackView.snp.top).offset(-40)
-            make.height.equalTo(50)
-        })
-
-        kakaoSignUpBUtton.snp.makeConstraints({make in
-            make.leading.equalToSuperview().offset(20)
-            make.trailing.equalToSuperview().offset(-20)
-            make.bottom.equalTo(appleSignUpButton.snp.top).offset(-10)
-            make.height.equalTo(50)
-        })
-        
         naverSignUpButton.snp.makeConstraints({make in
             make.centerX.equalToSuperview().offset(-60)
             make.top.equalTo(firstLine.snp.bottom).offset(50)
@@ -154,5 +229,25 @@ class SignUpView: UIView {
             make.top.equalTo(firstLine.snp.bottom).offset(70)
             make.height.equalTo(20)
         })
+        
+        alreadySignUpLabel.snp.makeConstraints({make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(secondLine.snp.bottom).offset(70)
+            make.height.equalTo(20)
+        })
+        
+        phoneNumLoginButton.snp.makeConstraints({make in
+            make.leading.equalToSuperview().offset(70)
+            make.trailing.equalToSuperview().offset(-70)
+            make.top.equalTo(alreadySignUpLabel.snp.bottom).offset(10)
+            make.height.equalTo(50)
+        })
+        
+        inquireButton.snp.makeConstraints({make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(phoneNumLoginButton.snp.bottom).offset(100)
+            make.height.equalTo(15)
+        })
     }
+
 }
