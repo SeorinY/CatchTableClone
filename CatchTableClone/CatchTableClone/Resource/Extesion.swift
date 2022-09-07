effb43ca2eec90df3c084a6237e761dd358bc850
유서린 <81281190+SeorinY@users.noreply.github.com>
2022-09-07 10:07:13 +0900

    [FEA] : Extension UIView

--- CatchTableClone/CatchTableClone/Resource/Extesion.swift
+++ CatchTableClone/CatchTableClone/Resource/Extesion.swift
@@ -22,4 +22,28 @@ extension UIImage {
         return scaledImage!
     }
 
+    
+}
+
+extension UIView{
+    public var width : CGFloat{
+        return frame.size.width
+    }
+    
+    public var height : CGFloat{
+        return frame.size.height
+    }
+    
+    public var top : CGFloat{
+        return frame.origin.y
+    }
+    public var bottom : CGFloat{
+        return frame.origin.y + frame.size.height
+    }
+    public var left : CGFloat{
+        return frame.origin.x
+    }
+    public var right : CGFloat{
+        return frame.origin.x + frame.size.width
+    }
 }
