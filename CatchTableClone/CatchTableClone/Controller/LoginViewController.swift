91bff30d887c3f21a75c60bf1c180dd1f911bfdb
유서린 <81281190+SeorinY@users.noreply.github.com>
2022-09-07 17:09:33 +0900

    [FEA] : Naver Login

--- CatchTableClone/CatchTableClone/Controller/LoginViewController.swift
+++ CatchTableClone/CatchTableClone/Controller/LoginViewController.swift
@@ -10,17 +10,24 @@ import SnapKit
 import KakaoSDKCommon
 import KakaoSDKUser
 import AuthenticationServices
+import NaverThirdPartyLogin
+import Alamofire
 
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController{
     private let loginLogoView = LoginLogoView()
     private let loginView = LoginView()
+    
+    
+    let naverLoginInstance = NaverThirdPartyLoginConnection.getSharedInstance()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(loginLogoView)
         view.addSubview(loginView)
         loginView.delegate = self
+
+        naverLoginInstance?.delegate = self
     }
     
     override func viewDidLayoutSubviews() {
@@ -76,13 +83,8 @@ extension LoginViewController : LoginVieWDelegate{
     }
     
     func naverLoginButtonPressed() {
-        let alert = UIAlertController(title: "'캐치테이블'에서 'NAVER'을(를) 열려고 합니다.", message: "", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
-        alert.addAction(UIAlertAction(title: "열기", style: .default) {_ in
-            self.navigationController?.pushViewController(UIViewController(), animated: true)
-        })
-        
-        self.present(alert, animated: true)
+        self.naverLoginInstance?.requestThirdPartyLogin()
+//        naverLoginInstance?.requestDeleteToken()    // 로그아웃
     }
     
     func questionmarkButtonPressed() {
@@ -136,4 +138,74 @@ extension LoginViewController : ASAuthorizationControllerPresentationContextProv
         // Handle error.
     }
     
+}
+extension LoginViewController : NaverThirdPartyLoginConnectionDelegate{
+    func naverLoginPaser() {
+              guard let accessToken = naverLoginInstance?.isValidAccessTokenExpireTimeNow() else { return }
+              
+              if !accessToken {
+                return
+              }
+              
+              guard let tokenType = naverLoginInstance?.tokenType else { return }
+              guard let accessToken = naverLoginInstance?.accessToken else { return }
+                
+              let requestUrl = "https://openapi.naver.com/v1/nid/me"
+              let url = URL(string: requestUrl)!
+              
+              let authorization = "\(tokenType) \(accessToken)"
+              
+              let req = AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: ["Authorization": authorization])
+              
+              req.responseJSON { response in
+                
+                guard let body = response.value as? [String: Any] else { return }
+                  
+                  if let resultCode = body["message"] as? String{
+                      if resultCode.trimmingCharacters(in: .whitespaces) == "success"{
+                          let resultJson = body["response"] as! [String: Any]
+                          
+                          let name = resultJson["name"] as? String ?? ""
+                          let id = resultJson["id"] as? String ?? ""
+                          let phone = resultJson["mobile"] as! String
+                          let gender = resultJson["gender"] as? String ?? ""
+                          let birthyear = resultJson["birthyear"] as? String ?? ""
+                          let birthday = resultJson["birthday"] as? String ?? ""
+                          let profile = resultJson["profile_image"] as? String ?? ""
+                          let email = resultJson["email"] as? String ?? ""
+                          let nickName = resultJson["nickname"] as? String ?? ""
+
+                          print("네이버 로그인 이름 ",name)
+                          print("네이버 로그인 아이디 ",id)
+                          print("네이버 로그인 핸드폰 ",phone)
+                          print("네이버 로그인 성별 ",gender)
+                          print("네이버 로그인 생년 ",birthyear)
+                          print("네이버 로그인 생일 ",birthday)
+                          print("네이버 로그인 프로필사진 ",profile)
+                          print("네이버 로그인 이메일 ",email)
+                          print("네이버 로그인 닉네임 ",nickName)
+                      }
+                      else{
+                          //실패
+                      }
+                  }
+              }
+        }
+ 
+    func oauth20ConnectionDidFinishRequestACTokenWithAuthCode() {
+        print("네이버 로그인 성공")
+        self.naverLoginPaser()
+    }
+    
+    func oauth20ConnectionDidFinishRequestACTokenWithRefreshToken() {
+        print("네이버 토큰\(naverLoginInstance?.accessToken)")
+    }
+    
+    func oauth20ConnectionDidFinishDeleteToken() {
+        print("네이버 로그아웃")
+    }
+    
+    func oauth20Connection(_ oauthConnection: NaverThirdPartyLoginConnection!, didFailWithError error: Error!) {
+        print("에러 = \(error.localizedDescription)")
+    }
 }
--- CatchTableClone/CatchTableClone/Resource/AppDelegate.swift
+++ CatchTableClone/CatchTableClone/Resource/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import KakaoSDKCommon
+import NaverThirdPartyLogin
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,12 +17,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         KakaoSDK.initSDK(appKey: "1e0080a4ac6b2bd610b850a97e4cc97b")
+        
+        application.registerForRemoteNotifications()
+        let instance = NaverThirdPartyLoginConnection.getSharedInstance()
+        instance?.isNaverAppOauthEnable = true //네이버앱 로그인 설정
+        instance?.isInAppOauthEnable = true //사파리 로그인 설정
+        
+        instance?.serviceUrlScheme = "1e0080a4ac6b2bd610b850a97e4cc97b" //URL Scheme
+        instance?.consumerKey = "rhn7U9xnOKhE7hkqLSC8" //클라이언트 아이디
+        instance?.consumerSecret = "ogfYguK7C3" //시크릿 아이디
+        instance?.appName = "CatchTableClone" //앱이름
+        
 
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+            NaverThirdPartyLoginConnection.getSharedInstance()?.application(app, open: url, options: options)
+            return true
+    }
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
