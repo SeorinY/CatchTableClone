7f0b58fb711469e68e6795e2416006ba76b810d7
Jung SeokYoung <thrdud0423@gmail.com>
2022-09-07 09:51:29 +0900

    [BLD] : Kakao Login setup
    카카오 로그인 초기설정

--- CatchTableClone/CatchTableClone/Resource/AppDelegate.swift
+++ CatchTableClone/CatchTableClone/Resource/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        KakaoSDK.initSDK(appKey: "1e0080a4ac6b2bd610b850a97e4cc97b")
+
         return true
     }
 
