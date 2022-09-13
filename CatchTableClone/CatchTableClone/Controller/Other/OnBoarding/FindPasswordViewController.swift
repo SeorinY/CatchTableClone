e5f2f19f12301c75c802231f131ef7c112bdb9b8
유서린 <81281190+SeorinY@users.noreply.github.com>
2022-09-13 15:37:31 +0900

    [UPT] : Find Password Controller

--- CatchTableClone/CatchTableClone/Controller/Other/OnBoarding/FindPasswordViewController.swift
+++ CatchTableClone/CatchTableClone/Controller/Other/OnBoarding/FindPasswordViewController.swift
@@ -13,32 +13,32 @@ import Then
 
 class FindPasswordViewController: UIViewController {
     private let explainLabel = UILabel().then {
-        $0.text = "케치테이블 가입 시 사용한 휴대폰 번호를 입력해주세요."
-        $0.font = UIFont.systemFont(ofSize:15)
+        $0.text = "캐치테이블 가입 시 사용한 휴대폰 번호를 입력해주세요."
+        $0.font = Font.generalFont
     }
     private let phoneNumTextField = CSTextField("숫자만 입력해 주세요.")
-    
     private let certificationButton = generalButton(.wait, "인증번호 요청")
-    
-    private let changePasswordButton = UIButton().then {
-        $0.setTitle("비밀번호 변경", for: .normal)
-        $0.setTitleColor(.secondaryLabel, for: .normal)
-        $0.backgroundColor = .secondarySystemBackground
-        $0.layer.cornerRadius = 7
-    }
+    private let changePasswordButton = generalButton(.wait, "비밀번호 변경")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        navigationLabel("비밀번호 변경")
+        addSubView()
+        setSubViewLayout()
+    }
+    private func addSubView(){
         view.addSubview(explainLabel)
         view.addSubview(phoneNumTextField)
+        phoneNumTextField.addTarget(self, action: #selector(checkPhoneNum), for: .editingChanged)
         view.addSubview(certificationButton)
         view.addSubview(changePasswordButton)
-        view.largeContentTitle = "비밀번호 찾기"
     }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
+    @objc private func checkPhoneNum(){
+        if phoneNumTextField.text?.count ?? 0 < 10 { certificationButton.styleConfigure(.wait) }
+        else { certificationButton.styleConfigure(.ready) }
+    }
+    private func setSubViewLayout() {
         explainLabel.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(100)
             make.leading.equalToSuperview().offset(20)
